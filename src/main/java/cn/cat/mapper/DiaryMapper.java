b62a222a99b61558674d6cdea8039d1f79f4d178
Administrator <1209306293@qq.com>
2018-07-23 18:46:16 +0800

    Diary开头

---
+++ src/main/java/cn/cat/mapper/DiaryMapper.java
@@ -0,0 +1,5 @@
+package cn.cat.mapper;
+
+public interface DiaryMapper {
+
+}
---
+++ src/main/java/cn/cat/pojo/Diary.java
@@ -0,0 +1,64 @@
+package cn.cat.pojo;
+
+import java.sql.Date;
+
+public class Diary {
+
+	private String diaryid;
+	private String catid;
+	private String catname;
+	private String userid;
+	private String username;
+	private Date posttime;
+	private String title;
+	private String content;
+	public String getDiaryid() {
+		return diaryid;
+	}
+	public void setDiaryid(String diaryid) {
+		this.diaryid = diaryid;
+	}
+	public String getCatid() {
+		return catid;
+	}
+	public void setCatid(String catid) {
+		this.catid = catid;
+	}
+	public String getCatname() {
+		return catname;
+	}
+	public void setCatname(String catname) {
+		this.catname = catname;
+	}
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public Date getPosttime() {
+		return posttime;
+	}
+	public void setPosttime(Date posttime) {
+		this.posttime = posttime;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+}
