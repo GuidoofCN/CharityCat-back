dc965a9faba766a340b6e6e6f2ee3b017b05c47d
hjh <497804227@qq.com>
2018-07-27 21:02:55 +0800

    2018-7-27

---
+++ src/main/java/cn/cat/pojo/CatNotePicPojo.java
@@ -0,0 +1,18 @@
+package cn.cat.pojo;
+
+public class CatNotePicPojo {
+	private String catid;
+	private String pic;
+	public String getCatid() {
+		return catid;
+	}
+	public void setCatid(String catid) {
+		this.catid = catid;
+	}
+	public String getPic() {
+		return pic;
+	}
+	public void setPic(String pic) {
+		this.pic = pic;
+	}
+}
